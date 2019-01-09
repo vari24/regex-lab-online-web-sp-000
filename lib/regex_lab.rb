080623b27250e0d3a0b4b91ced23c8dcc056838f
Sara Varisara <varisara.n@gmail.com>
2019-01-09 07:29:34 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match(/\A[aeiouAEIOU]/)
-    return true
-  else
-    return false
-end
+  word.match(/\A[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
