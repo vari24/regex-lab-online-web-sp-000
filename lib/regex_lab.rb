f99594d0f5352f46fd398833fe2c0307b24127eb
Sara Varisara <varisara.n@gmail.com>
2019-01-09 07:28:58 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,25 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\A[aeiouAEIOU]/)
+    return true
+  else
+    return false
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un+\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z]/) && text.match(/[ _.,!"']$/) ? true : false
 end
 
 def valid_phone_number?(phone)
+  phone.match(/\(?\d{3}[) ]?\d{3}[- ]?\d{4}$/) ? true : false
+  end
+  
 
-end
